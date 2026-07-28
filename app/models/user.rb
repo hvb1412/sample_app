42894cf7f821955de51fdfe56a102b842eba23df
hvb1412 <hoangvanbinh141205@gmail.com>
2026-07-28 10:06:16 +0700

    Chapter 6

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = Regexp.new(Settings.user.email.regex, Regexp::IGNORECASE)
+
+  before_save :downcase_email
+
+  validates :name, presence: true,
+    length: {maximum: Settings.user.name.max_length}
+  validates :email, presence: true,
+    length: {maximum: Settings.user.email.max_length},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+
+  has_secure_password
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+end
