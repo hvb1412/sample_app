fdbc459f4748239e22a2c38515f0848c4e2e6a8e
hvb1412 <hoangvanbinh141205@gmail.com>
2026-07-30 10:56:17 +0700

    Chapter 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,14 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params.dig(:session, :email)&.downcase)
-    if user&.authenticate(params.dig(:session, :password))
+    user = find_user
+    if user.try(:authenticate, params.dig(:session, :password))
       log_in user
+      params.dig(:session, :remember_me) == "1" ? remember(user) : forget(user)
       flash[:success] = t("flash.success.login")
       redirect_to user_path(user, locale: I18n.locale)
     else
-      flash.now[:danger] = t("flash.danger.invalid_email_password")
-      render :new
+      render_login_error
     end
   end
 
@@ -17,5 +17,16 @@ class SessionsController < ApplicationController
     log_out
     flash[:success] = t("flash.success.logout")
     redirect_to root_path(locale: I18n.locale)
+  end
+
+  private
+
+  def find_user
+    User.find_by(email: params.dig(:session, :email)&.downcase)
+  end
+
+  def render_login_error
+    flash.now[:danger] = t("flash.danger.invalid_email_password")
+    render :new
   end
 end
